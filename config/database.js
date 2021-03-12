ccabb9645fd4e01b964be4f15bcc86a877019a85
William Phokompe <phokompe.william@outlook.com>
2021-03-12 23:36:40 +0200

    create mongodb strapi

---
+++ config/database.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', 'giyantcluster.2ok45.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'strapi-cms'),
+        username: env('DATABASE_USERNAME', 'super'),
+        password: env('DATABASE_PASSWORD', 'giyant'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+    },
+  },
+});
