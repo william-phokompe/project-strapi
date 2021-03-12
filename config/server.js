516c80612b764924c275a72b0d93500376f9628a
William Phokompe <phokompe.william@outlook.com>
2021-03-12 22:16:21 +0200

    delete

--- config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '38fec50d70af35ab500cfc11ea3aef8c'),
-    },
-  },
-});
